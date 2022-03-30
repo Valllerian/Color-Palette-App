95c58ce4c11be291fefb53ff8b4a6b6ecc0f797b
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-29 22:00:06 -0600

    Random color on click works

--- src/Color/Color.jsx
+++ src/Color/Color.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import './color.css'
 const Color = (props) => {
+const [newColor, setNewColor] = useState()
   let random = props.color;
 
   const newRandom = () => {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    let newColor = "#" + randomColor;
-    return newColor
+    let color = "#" + randomColor;
+    setNewColor(color)
+    return 
   }
 
 //   newColor ? random=newColor : console.log("hi")
-
+ 
   return (
     <div id="test">
       <div
-        style={{ backgroundColor: random }}
+        style={{ backgroundColor: (newColor?newColor:random) }}
         className="text-white h-40 w-40 tile"
         onClick={newRandom}
       ></div>
